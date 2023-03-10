b8e4c67f2b9cc2d5065355227509be7be142456f
Friederike Schertel <friederike@schertel.org>
2023-03-10 21:10:59 +0100

    frage

--- lvl01.ende.sh
+++ lvl01.ende.sh
@@ -2,9 +2,21 @@ cat << EOF
 ------------------
  Lernbash Level 1
 ------------------
+EOF
 
-BEENDET!
 
+cat << EOF 
+Okay, du bist fertig mit ausprobieren.
+Kannst du mir sagen was der Befehl pwd tut?
+
+A: eins
+B: zwei
+EOF
+
+read -r pwd
+
+cat << EOF
+$pwd
 Das ist vorerst das Ende von Lernbash. 
 Danke für's mitmachen. :)
 
