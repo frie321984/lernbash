5e1f3c36b4fe697ba0dccb658d947f87ef4ca903
Friederike Schertel <friederike@schertel.org>
2023-03-11 23:47:10 +0100

    liste alle level

--- hilfe.sh
+++ hilfe.sh
@@ -13,8 +13,12 @@ Oh du steckst fest? Vielleicht hilft dir das weiter:
 
 Diese Kapitel kannst du starten:
 
-lvl1
-lvl2
+EOF
+for dir in "$HOME/lernbash/level/*"; do
+    lvl=$(basename "$dir")
+    echo "lvl$lvl"
+done
+cat << EOF
 
 Zeige diese Hilfe nochmal an mit:
 
