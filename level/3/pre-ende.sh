b307a3a77e6dd9814781b6b93a5e71bcd1466510
Friederike Schertel <friederike@schertel.org>
2023-03-11 23:29:19 +0100

    pre-ende.sh

--- level/3/pre-ende.sh
+++ level/3/pre-ende.sh
@@ -2,11 +2,12 @@
 
 if [ -d ~/texte ]; then
     # aufgabe erfolgreich geschafft!
-    exit 0;
+    echo "Geschafft!"
+    exit 0
 else
     echo "Du hast noch nicht den Ordner texte angelegt."
     echo "Probiere es mal mit:"
     echo
     echo "mkdir texte"
-    exit 1;
+    exit 1
 fi
