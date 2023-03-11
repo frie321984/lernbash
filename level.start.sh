1aa1fb2bf91dd735b9fbe864a763854f12970185
Friederike Schertel <friederike@schertel.org>
2023-03-11 22:37:59 +0100

    duh... echo statt cat natürlich

--- level.start.sh
+++ level.start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 clear
-echo "\$1: $1"
-cat "$1" > ~/.lb/current-level
+echo "$1" > ~/.lb/current-level
 cp -p "$HOME/lernbash/level.ende.sh" ~/.fertig
 
 cat "$HOME/lernbash/level/$1/start.txt"
