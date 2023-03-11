11f95f1987a2f7f3697d192b81aff1ca9a3430d0
Friederike Schertel <friederike@schertel.org>
2023-03-11 21:43:04 +0100

    ausprobieren alles zu einem kapitel in einer datei?

--- lvl03.sh
+++ lvl03.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-cp ~/lernbash/lvl03.ende.sh ~/.fertig
 
 cat << EOF
  █   ██▀ █ █ ██▀ █     ▀██
@@ -26,4 +25,23 @@ Wenn du soweit bist, beende das Level
 wieder mit dem Befehl
 fertig
 
+EOF
+
+cat >> ~/.fertig << EOF
+#!/bin/bash
+clear
+
+rm -f ~/.fertig
+touch ~/.lb/fertig/lvl03
+
+ls
+
+cat << EC
+
+Du hast Level 3 geschafft.
+
+Das ist vorerst das Ende von Lernbash.
+Ich hoffe es hat dir Spaß gemacht.
+Vielen Dank für's Mitmachen. :)
+EC
 EOF
