93488763d99597975e462e40ed9cba27c25b6378
Friederike Schertel <friederike@schertel.org>
2023-03-11 23:27:16 +0100

    pre-ende.sh

--- level.ende.sh
+++ level.ende.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+clear
 if [ -f "$HOME/.lb/current-level" ]; then
     currentLevel=$(cat "$HOME/.lb/current-level")
 else
-    clear
+    # es gibt gerade kein aktives level
     ~/lernbash/geht-nicht.sh
     exit 0
 fi
 
-clear
-# todo das mit dem .fertig ist noch zu umständlich und kann
-# jetzt sicher vereinfacht werden.
+# execute pre-ende if exists
+if [ -f "$HOME/lernbash/level/$currentLevel/pre-ende.sh" ]; then
+    . "$HOME/lernbash/level/$currentLevel/pre-ende.sh"
+
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        echo "Du kannst das Level leider noch nicht beenden."
+        exit 0
+    fi
+fi
+
 rm -f $HOME/.fertig
 rm "$HOME/.lb/current-level"
 touch "$HOME/.lb/fertig/$currentLevel"
 
-# TODO execute pre-ende if exists
 if [ -f "$HOME/lernbash/level/$currentLevel/ende.txt" ]; then
     cat "$HOME/lernbash/level/$currentLevel/ende.txt"
 else
     echo "Level $currentLevel ist leider kaputt. Konnte ende.txt nicht finden."
 fi
+
 # TODO execute post-ende if exists
