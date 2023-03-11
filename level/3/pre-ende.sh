1014d2fff055518212c9bb75fe9f3e60686408a2
Friederike Schertel <friederike@schertel.org>
2023-03-11 23:29:54 +0100

    darf nicht rausspringen!

--- level/3/pre-ende.sh
+++ level/3/pre-ende.sh
@@ -3,7 +3,6 @@
 if [ -d ~/texte ]; then
     # aufgabe erfolgreich geschafft!
     echo "Geschafft!"
-    exit 0
 else
     echo "Du hast noch nicht den Ordner texte angelegt."
     echo "Probiere es mal mit:"
