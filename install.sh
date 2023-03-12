fdebccd378035cb35a419aaabe4c049d06ced360
Friederike Schertel <friederike@schertel.org>
2023-03-12 09:34:18 +0100

    scriptpath bei installation

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 # schon ~/lernbash/ geklont wurde wie in SETUP.md
 # beschrieben.
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo $SCRIPTPATH
 mkdir -p ~/.lb/fertig
 mv -i ~/.bashrc ~/.bashrc.original
 ln -s ~/lernbash/.bashrc ~/.bashrc
