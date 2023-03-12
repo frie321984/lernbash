a147f431ae4cdaae2d8bd416d6850ce8022de299
Friederike Schertel <friederike@schertel.org>
2023-03-12 11:12:37 +0100

    check in place

--- level/4/pre-ende.sh
+++ level/4/pre-ende.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo $(basename ${PWD})
-
-exit 1
+if [ "$(basename ${PWD})" == "texte" ]; then
+    # alles gut. do nothing.
+else
+    echo "Du bist noch nicht im texte-Ordner sondern hier..."
+    pwd
+    echo 
+    echo "Probiere es nochmal mit "
+    echo "cd texte"
+    exit 1
+fi
