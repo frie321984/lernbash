8373096b14f0dc49c18350e5393c9060b6fa3e55
Friederike Schertel <friederike@schertel.org>
2023-03-12 10:09:31 +0100

    frag nich so viel

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "scriptpath: $SCRIPTPATH"
 echo "$SCRIPTPATH" > "$HOME/.lernbashpath"
 mkdir -p ~/.lb/fertig
-mv -i ~/.bashrc ~/.bashrc.original
+mv --backup ~/.bashrc ~/.bashrc.original
 ln -s ~/lernbash/.bashrc ~/.bashrc
