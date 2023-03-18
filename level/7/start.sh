63b74a6593ba6f7f3948ab010c27dd0c807c9065
Friederike Schertel <friederike@schertel.org>
2023-03-18 13:13:14 +0100

    struktur geändert

--- level/7/start.sh
+++ level/7/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-rm -rf ./level7
-cp -r "$lernbashpath/level/7/dungeon/" ./level7
+rm -rf ./raum
+cp -r "$lernbashpath/level/7/dungeon/raum/" ./raum
+cp -r "$lernbashpath/level/7/dungeon/zettel" .
 
-cd level7
 cat zettel
