8878cd18cc3bb40514bcb43773bec35381a48aa4
Friederike Schertel <friederike@schertel.org>
2023-03-19 19:41:22 +0100

    Klarer beschreiben

--- level/6/start.sh
+++ level/6/start.sh
@@ -5,8 +5,9 @@ Hallo! Cool dass du mich lesen kannst.
 
 Dafür verrate ich dir eine Geheimzahl: 42
 
-Damit kannst du das Level jetzt beenden. ;)
-
+Merke sie dir gut. 
+Du kannst das Level jetzt beenden mit
+fertig
 EOF
 
 cat << EOF
@@ -14,7 +15,13 @@ cat << EOF
 *es raschelt leise und ein Papier fällt auf den Boden*
 
 Oh ein Zettel. Du kannst ihn lesen mit 
-dem Befehl cat.
+dem Befehl 
 
-Schau dich hier noch einmal um und lies den Zettel.
+cat DATEINAME
+
+Statt DATEINAME gibst du den Namen der Datei an
+die du lesen möchtest. 
+
+Schau dich hier noch einmal um mit ls und lies dann
+den Zettel.
 EOF
