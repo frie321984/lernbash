c1527885689eb6d3b2d3f967a502f0031185d13d
Friederike <friederike@schertel.org>
2023-03-19 14:01:21 +0100

    nextlevel

--- level.start.sh
+++ level.start.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 clear
-echo "$1" > ~/.lb/current-level
+
+currentLevel=$1
+echo "$currentLevel" > ~/.lb/current-level
+(( nextLevel = currentLevel + 1 ))
+echo $nextLevel
 cp -p "$lernbashpath/level.ende.sh" ~/.fertig
 
 echo "-------------"
-echo "| Level $1 |"
+echo "| Level $currentLevel |"
 echo "-------------"
 
-if [ -f "$lernbashpath/level/$1/start.sh" ]; then
+if [ -f "$lernbashpath/level/$currentLevel/start.sh" ]; then
 
-    . "$lernbashpath/level/$1/start.sh"
-elif [ -f "$lernbashpath/level/$1/start.txt" ]; then
-    cat "$lernbashpath/level/$1/start.txt"
+    . "$lernbashpath/level/$currentLevel/start.sh"
+elif [ -f "$lernbashpath/level/$currentLevel/start.txt" ]; then
+    cat "$lernbashpath/level/$currentLevel/start.txt"
 
 else
     echo "Level $1 ist leider kaputt... Konnte die start-datei nicht finden."
