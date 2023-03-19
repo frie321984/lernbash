2705c14627c05f3a05aeab24479b1a3d4a17abc6
Friederike Schertel <friederike@schertel.org>
2023-03-19 19:43:44 +0100

    tab erklären

--- level/7/start.sh
+++ level/7/start.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 cat << EOF > ach-du-schande--so-ein-langer-dateiname
+
 Bravo! Die Geheimzahl lautet: 36
+
+Beende jetzt das Level mit 
+fertig
+
 EOF
 
 cat << EOF
 Du kannst jetzt mit cat DATEINAME Dateien lesen.
 
-Bei kurzen DATEINAMEN ist es kein Problem
-das zu tippen. Vielleicht hast du schon 
-gemerkt, dass viele Befehle schön kurz sind?
-Das liegt daran, dass wir Computermenschen oft
-faul sind. ;)
+Bei kurzen DATEINAMEN ist es kein Problem das zu
+tippen. Vielleicht hast du schon gemerkt, dass
+viele Befehle schön kurz sind? Das liegt daran,
+dass wir Computermenschen oft faul sind. ;)
 
 Bei langen Dateinamen kann es aber echt nerven.
-Dafür gibt es den Tabulator.
+Dafür gibt es den Tabulator. Das ist die Taste 
+auf der Tastatur mit zwei Pfeilen übereinander:
+|<--
+-->|
 
 Probiere es mal aus. Tippe 
 cat a
-und drücke dann die TAB Taste. Das Terminal 
-wird jetzt den Dateinamen vervollständigen.
-Drücke dann Enter um die Datei zu lesen.
+und drücke dann die TAB Taste. Das Terminal wird
+jetzt den Dateinamen vervollständigen. Drücke
+dann Enter um die Datei zu lesen.
 EOF
