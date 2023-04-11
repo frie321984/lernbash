c87219d23a98c2cb476eb9d7aa02c7cb970c55b6
Friederike Schertel <friederike@schertel.org>
2023-04-11 11:49:22 +0200

    nochmal nachfragen

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "scriptpath: $SCRIPTPATH"
+
+read -n1 -p "Deine .bashrc Datei wird ersetzt. Weitermachen (y|N)? " doit
+
+if [ doit != 'y' ]; then
+	echo "Abbruch"
+	exit 1	
+fi
+
 echo "$SCRIPTPATH" > "$HOME/.lernbashpath"
 mkdir -p ~/.lb/fertig
 mv -i ~/.bashrc ~/.bashrc.original
