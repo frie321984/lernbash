789a27c83dda787938aa5126c0d2d0e3f6e2f67c
Friederike Schertel <friederike@schertel.org>
2024-02-02 17:51:03 +0100

    OMG ES FUNKTIONIERT ALLES!!!

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,13 @@ FROM docker.io/library/nginx:1.17.1-alpine
 LABEL authors="@frie321984"
 
 RUN apk --no-cache add bash
+RUN apk --no-cache add tree
 RUN adduser -D -u 1000 hera
-
 
 COPY ./src/game /home/hera/.lernbash
 RUN chown -R hera:hera /home/hera/.lernbash
 
 USER hera
 WORKDIR /home/hera
+RUN /home/hera/.lernbash/install.sh
 ENTRYPOINT ["bash"]
--- src/game/install.sh
+++ src/game/install.sh
@@ -10,19 +10,12 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 echo "scriptpath: $SCRIPTPATH"
 
-read -n1 -p "Deine .bashrc Datei wird ersetzt. Weitermachen (y|N)? " doit
-
-if [ "$doit" != 'y' ]; then
-	echo "Abbruch"
-	exit 1	
-fi
-
 echo "$SCRIPTPATH" > "$HOME/.lernbashpath"
 mkdir -p ~/.lb/fertig
-mv -i ~/.bashrc ~/.bashrc.original
+~/.bashrc && mv -i ~/.bashrc ~/.bashrc.original
 ln -sf "$SCRIPTPATH/.bashrc" ~/.bashrc
 
 # programme die installiert sein sollten
 tree --version
-cat --version
+cat
 pwd
